a93457501c657749a76c5c0983d69a35fae6d4e5
e2300113 <kadam.iut@gmail.com>
2023-11-24 10:05:03 +0100

    24/11

---
+++ Ex5.py
@@ -0,0 +1,43 @@
+import re
+
+num_format = re.compile(r"^\-?[0-9][0-9]*$")
+h1, h2=-1, -1
+cost=0
+t1=0
+t2=0
+
+def verif(x):
+    while True:
+        if not (re.match(num_format, x)):
+            print("Valeur incorrect!")
+            return -1
+        else:
+            x=int(x)
+            if x>=0 and x<=24:
+                return x
+            else:
+                print("Valeur incorrect!")
+                return -1
+
+while h1==-1:
+    h1=input("Donnez l’heure de début de la location (un entier) : ")
+    h1=int(verif(h1))
+while h2==-1:
+    h2=input("Donnez l’heure de fin de la location (un entier) : ")
+    h2=int(verif(h2))
+print("Vous avez loué votre vélo pendant")
+
+for i in range(h1,h2):
+    if i>=7 and i<17:
+        t2+=1
+        cost+=2
+    else:
+        t1+=1
+        cost+=1
+print(f"{t1} heure(s) au tarif horaire de 1.0 euro(s)")
+print(f"{t2} heure(s) au tarif horaire de 2.0 euro(s)")
+print(f"Le montant total à payer est de {cost}.0 euro(s).")
+
+
+
+
